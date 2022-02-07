5b91f698053bd550e6ee4c36708a3590c58b1d96
KELONMYOSA <79825223592@yandex.ru>
2022-02-08 00:20:19 +0300

    added a couple of lines of code

--- src/test/java/tests/FormTest.java
+++ src/test/java/tests/FormTest.java
@@ -1,4 +1,30 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
 public class FormTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void FillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Name");
+        $("#lastName").setValue("LastName");
+        $("#userEmail").setValue("test@mail.ru");
+        //$("#gender-radio-1.custom-control-input").click();
+        $("#userNumber").setValue("0123456789");
+        //$("#dateOfBirth").setValue("01 01 2000");
+        //$("#subjectsContainer").setValue("informatics");
+
+    }
 }
